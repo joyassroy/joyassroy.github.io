0d35e6e74ce305522c732ca308582b7605cdf8d2
jb <joyassroy@Joyassroys-MacBook-Air.local>
2026-03-01 23:43:15 +0600

    deoployment done

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // এই লাইনটি অবশ্যই যোগ করুন
+  images: {
+    unoptimized: true, // স্ট্যাটিক হোস্টিংয়ের জন্য এটি দরকার
+  },
   /* config options here */
 };
 
